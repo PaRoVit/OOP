4b88d98479fa5e3c69c68ecf66e050c11bc0c37f
Pavel Rozhkov <mai.parovit@mail.ru>
2024-09-10 22:28:17 +0300

    update tests

--- test/lrTests.cpp
+++ test/lrTests.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "../include/plantGrowth.h"
 
-TEST(plantGrowthTest, ReturnsCorrectResultFor100To10)
+TEST(CountOnesInRangeTest1, ReturnsCorrectResultFor100To10)
 {
     int upSpeed = 100;
     int downSpeed = 10;
@@ -10,7 +10,7 @@ TEST(plantGrowthTest, ReturnsCorrectResultFor100To10)
     ASSERT_TRUE(result == 10);
 }
 
-TEST(plantGrowthTest, ReturnsCorrectResultFor100To10)
+TEST(CountOnesInRangeTest2, ReturnsCorrectResultFor100To10)
 {
     int upSpeed = 100;
     int downSpeed = 10;
@@ -19,7 +19,7 @@ TEST(plantGrowthTest, ReturnsCorrectResultFor100To10)
     ASSERT_TRUE(result == 1);
 }
 
-TEST(plantGrowthTest, ReturnsCorrectResultForRange60To50)
+TEST(CountOnesInRangeTest, ReturnsCorrectResultForRange60To50)
 {
     int upSpeed = 60;
     int downSpeed = 50;
